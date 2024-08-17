e0c1c52b97e6d9aea5a6783d24f3715508d95c63
Anvar Nazar <anvarnasar@ymail.com>
2024-08-17 22:42:19 +0530

    dap php setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-dap.nvim" },
 	config = function ()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		require('telescope').load_extension('dap')
 	end
 }
